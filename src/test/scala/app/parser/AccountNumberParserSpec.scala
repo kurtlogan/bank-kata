394830e4928bbae1baf9d0e07f83591d0c98dfdc
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-03-31 10:31:54 +0100

    reordered code in test

--- src/test/scala/app/parser/AccountNumberParserSpec.scala
+++ src/test/scala/app/parser/AccountNumberParserSpec.scala
@@ -3,21 +3,6 @@ package app.parser
 import app.{ Number, NumbersGenerator, UnitSpec }
 
 class AccountNumberParserSpec extends UnitSpec with NumbersGenerator {
-  implicit def numberToString(n: app.Number): String = app.Number.asString(n)
-
-  private def accountNumbersAsString(xs: List[AccountNumber]): String =
-    xs match {
-      case List()         ⇒ ""
-      case head :: List() ⇒ accountNumberAsString(head)
-      case head :: tail ⇒
-        accountNumberAsString(head) + "\n" + accountNumbersAsString(tail)
-    }
-
-  private def accountNumberAsString(a: AccountNumber): String =
-    s"${Number.asString(a.n1)}${Number.asString(a.n2)}${Number.asString(a.n3)}\n" +
-      s"${Number.asString(a.n4)}${Number.asString(a.n5)}${Number.asString(a.n6)}\n" +
-      s"${Number.asString(a.n7)}${Number.asString(a.n8)}${Number.asString(a.n9)}\n" +
-      (" " * 27)
 
   "parse" should {
     "parse account number" in {
@@ -36,4 +21,18 @@ class AccountNumberParserSpec extends UnitSpec with NumbersGenerator {
       }
     }
   }
+
+  private def accountNumbersAsString(xs: List[AccountNumber]): String =
+    xs match {
+      case List()         ⇒ ""
+      case head :: List() ⇒ accountNumberAsString(head)
+      case head :: tail ⇒
+        accountNumberAsString(head) + "\n" + accountNumbersAsString(tail)
+    }
+
+  private def accountNumberAsString(a: AccountNumber): String =
+    s"${Number.asString(a.n1)}${Number.asString(a.n2)}${Number.asString(a.n3)}\n" +
+      s"${Number.asString(a.n4)}${Number.asString(a.n5)}${Number.asString(a.n6)}\n" +
+      s"${Number.asString(a.n7)}${Number.asString(a.n8)}${Number.asString(a.n9)}\n" +
+      (" " * 27)
 }
